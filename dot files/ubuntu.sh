b76daff39ff04e4e03fada51e18686c26db52800
Jess <d1al.t0ne@outlook.com>
2022-01-27 10:16:05 -0500

    Create ubuntu.sh

---
+++ dot files/ubuntu.sh
@@ -0,0 +1,13 @@
+sudo apt-get upgrade
+
+sudo apt-get update
+
+sudo apt-get install -y \
+git \
+golang-go \
+pip \
+
+# Color scheme
+sudo apt-get install dconf-cli
+$ git clone https://github.com/dracula/gnome-terminal
+$ cd gnome-terminal
