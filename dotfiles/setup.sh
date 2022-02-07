75e94354be351edcc75928202cc5d23f6cbc610a
Jess <d1al.t0ne@outlook.com>
2022-02-07 11:51:47 -0500

    Added findutils

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -4,8 +4,9 @@
 apt-get install -y \
 default-jdk \
 dnsutils \
+findutils \
 npm
 
 # Finish Environment Setup
 mv /dotfiles/.vimrc ~/
-mv /dotflis/.basrch ~/ and source .bashrc
+mv /dotflis/.basrch ~/ and source ~/.bashrc
