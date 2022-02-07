3860f9cc4c962cbfd5560a63c6de23409d349c72
Jess <d1al.t0ne@outlook.com>
2022-02-07 11:29:23 -0500

    Update and rename ubuntu.sh to setup.sh

---
+++ dotfiles/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Need for other tools and require interaction
+apt-get install -y \
+default-jdk \
+npm
+
+# Finish Environment Setup
+mv /dotfiles/.vimrc ~/
+mv /dotflis/.basrch ~/ and source .bashrc
