6ecd5fd48d76941b8471e41d4b9649d037a7e356
Jess <d1al.t0ne@outlook.com>
2022-02-07 14:52:15 -0500

    Update setup.sh

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Need for other tools and require interaction
-apt-get install -y \
-default-jdk \
-dnsutils \
-findutils \
-npm \
-vim-gtk3
-
 # Finish Environment Setup
 mv /dotfiles/.vimrc ~/
 mv /dotflis/.basrch ~/ and source ~/.bashrc
